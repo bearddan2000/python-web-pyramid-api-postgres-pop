a243c7125f1e5a436bc5ca4481ed2974719a4b35
bearddan2000 <bearddan2000@yahoo.com>
2023-03-04 00:14:23 -0500

    initial commit

---
+++ py-srv/bin/settings.py
@@ -0,0 +1,35 @@
+user = 'maria'
+password = 'pass'
+host = 'db'
+database = 'beverage'
+
+COCKROACH = {
+    'engine': 'cockroachdb',
+    'username': 'root',
+    'password': '',
+    'host': host,
+    'db_name': database,
+}
+
+MYSQL = {
+    'engine': 'mariadb+pymysql',
+    'username': user,
+    'password': password,
+    'host': host,
+    'db_name': database,
+}
+
+POSTGRESQL = {
+    'engine': 'postgresql',
+    'username': user,
+    'password': password,
+    'host': host,
+    'db_name': database,
+}
+
+SQLALCHEMY = {
+  'autocommit': False,
+  'autoflush': False,
+  'sessionmaker': [False, False],
+  'debug': False,
+}
